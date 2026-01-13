7241585c2d52316e209123df3020f043ef3a565d
Maria Fructuoso <marifructuosog718@gmail.com>
2026-01-13 12:46:12 +0100

    home tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // <--- IMPORTANTE: Importar esto
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule], // <--- IMPORTANTE: Añadirlo aquí
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // Variable para guardar qué formulario está abierto (login, register o null)
-  activeForm: 'login' | 'register' | null = null;
-
-  // Función para abrir/cerrar los formularios
-  toggleForm(formName: 'login' | 'register') {
-    // Si pulsamos el mismo que ya está abierto, lo cerramos (null)
-    if (this.activeForm === formName) {
-      this.activeForm = null;
-    } else {
-      // Si no, abrimos el nuevo
-      this.activeForm = formName;
-    }
-  }
 }
