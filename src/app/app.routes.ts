ff9edf9302327e724d860e155a144f062a516c9c
Maria Fructuoso <marifructuosog718@gmail.com>
2026-02-16 19:07:52 +0100

    Cambios en estilo: feed-page.html home-page.html landing-page.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home-page';
 import { FeedComponent } from './pages/feed/feed-page'; // Importa el nuevo
+import { LandingComponent } from './pages/landing/landing-page';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'feed', component: FeedComponent },
+  { path: 'landing', component: LandingComponent },
   { path: '**', redirectTo: '' } // Por si escriben una ruta que no existe
 ];      
